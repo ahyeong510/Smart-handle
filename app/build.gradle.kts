74abfeb8b33719e5cdbea5904d71f82220046e5c
anxncjswp5-png <anxncjswp5@gmail.com>
2025-09-23 19:45:58 +0900

    initial commit
    # Conflicts:
    #	.idea/gradle.xml
    #	.idea/misc.xml
    #	app/build.gradle.kts
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/res/values/strings.xml
    #	app/src/main/res/values/themes.xml
    #	gradle/wrapper/gradle-wrapper.properties
    #	settings.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.smart_handle"
+    namespace = "com.example.bike"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.smart_handle"
+        applicationId = "com.example.bike"
         minSdk = 26
         targetSdk = 36
         versionCode = 1

8518e97a9f33834912c92bc9497ad098248109a0
ahyeong510 <dkdud2276@kyonggi.ac.kr>
2025-10-07 22:37:07 +0900

    merge: resolve UI conflicts and combine screens

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,23 +27,25 @@ android {
             )
         }
     }
+
     compileOptions {
+        // 👉 팀이 Java 17 쓸 수 있다면 VERSION_17로 올려!
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
-    composeOptions{
+    composeOptions {
         kotlinCompilerExtensionVersion = "1.5.14"
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -52,8 +54,7 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
-    implementation("androidx.compose.ui:ui:<compose-version>")
-    implementation("androidx.compose.material3:material3:<version>")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -61,5 +62,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
     implementation("androidx.navigation:navigation-compose:2.7.0")
 }
